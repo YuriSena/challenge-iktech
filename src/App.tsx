afd4c2f75c069da46bd48a875bbf7b7b1a94f94d
YuriSena <ybsena@inf.ufpel.edu.br>
2022-01-20 23:07:14 -0300

    map items display on the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 
-import "./global.scss";
+import "./styles/global.scss";
 
 function App() {
   return (
